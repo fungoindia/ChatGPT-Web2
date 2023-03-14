827dd645efa3f9ee68002dde9c5a4811fc395270
fungoindia <83899086+fungoindia@users.noreply.github.com>
2023-03-14 10:57:54 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'jack',
+      description: '欢迎使用（官方ChatGPT 3.5Turbo接口）',
     },
   }
 }
